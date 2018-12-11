117a5da4550d2f04d8be3cdc4af33c7ecd9c1ed4
smart <caixuejun@growingio.com>
2018-12-12 00:39:31 +0800

    add pascal triangle

---
+++ PascalTriangle/pascal_triangle.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func makePascalTriangle(numRows int) [][]int {
+	if numRows < 1 {
+		return [][]int{}
+	}
+
+	res := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		num := i + 1
+		arr := make([]int, num)
+		arr[0] = 1
+		if num-1 > 0 {
+			arr[num-1] = 1
+		}
+		for j := 1; j < i; j++ {
+			pre := res[i-1]
+			arr[j] = pre[j-1] + pre[j]
+		}
+		res[i] = arr
+	}
+
+	return res
+}
+
+func main() {
+	numRows := 5
+	res := makePascalTriangle(numRows)
+	for _, v := range res {
+		fmt.Println(v)
+	}
+}
