23be01c0be3f723b0585aedc55ba2d49a5b5bd8c
smart <caixuejun@growingio.com>
2019-05-26 19:51:13 +0800

    add 538

---
+++ leetcode/leetcode538/convert_bst_to_greater_tree.go
@@ -0,0 +1,47 @@
+package leetcode538
+
+import (
+	. "algocasts/datastructure"
+)
+
+func dfs(root *TreeNode, sum int) int {
+	if root == nil {
+		return sum
+	}
+
+	cur := dfs(root.Right, sum)
+	root.Val += cur
+	return dfs(root.Left, root.Val)
+}
+
+func convertBST(root *TreeNode) *TreeNode {
+	dfs(root, 0)
+	return root
+}
+
+func convertBSTIterative(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	cur := root
+	st := make([]*TreeNode, 0)
+	sum := 0
+
+	for cur != nil || len(st) != 0 {
+		for cur != nil {
+			st = append(st, cur)
+			cur = cur.Right
+		}
+
+		cur = st[len(st)-1]
+		st = st[:len(st)-1]
+
+		cur.Val += sum
+		sum = cur.Val
+
+		cur = cur.Left
+	}
+
+	return root
+}
---
+++ leetcode/leetcode538/convert_bst_to_greater_tree_test.go
@@ -0,0 +1,24 @@
+package leetcode538
+
+import (
+	. "algocasts/datastructure"
+	"testing"
+)
+
+func TestConvertBST(t *testing.T) {
+	type testFunc func(*TreeNode) *TreeNode
+
+	testBody := func(f testFunc, t *testing.T) {
+		nums := []interface{}{5, 2, 13}
+		root := BuildTree(nums)
+		root = f(root)
+		if root.Val != 18 ||
+			root.Left.Val != 20 ||
+			root.Right.Val != 13 {
+			t.Error("20, 18, 13")
+		}
+	}
+
+	testBody(convertBST, t)
+	testBody(convertBSTIterative, t)
+}
